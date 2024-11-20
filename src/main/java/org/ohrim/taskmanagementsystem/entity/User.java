0806317bca9731fd93df6c594fc024b12e836622
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-20 21:15:00 +0300

    feat: user entity first implementation

---
+++ src/main/java/org/ohrim/taskmanagementsystem/entity/User.java
@@ -0,0 +1,40 @@
+package org.ohrim.taskmanagementsystem.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.ohrim.taskmanagementsystem.entity.user.Role;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+
+}
