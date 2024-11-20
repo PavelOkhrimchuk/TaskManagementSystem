5273efbf8bbbd31ed70ce0ddf97a32c8c15adf88
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-20 21:15:13 +0300

    feat: task entity first implementation

---
+++ src/main/java/org/ohrim/taskmanagementsystem/entity/Task.java
@@ -0,0 +1,50 @@
+package org.ohrim.taskmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.ohrim.taskmanagementsystem.entity.task.Priority;
+import org.ohrim.taskmanagementsystem.entity.task.Status;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "executor_id")
+    private User executor;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @Column(nullable = false)
+    private Instant updatedAt = Instant.now();
+}
