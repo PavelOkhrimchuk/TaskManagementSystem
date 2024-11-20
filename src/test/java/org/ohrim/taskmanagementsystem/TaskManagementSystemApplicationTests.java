d6d10d09a5d60d4d7ca4b65f4688fe752ce15a3b
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-20 18:08:55 +0300

    Initial commit

---
+++ src/test/java/org/ohrim/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package org.ohrim.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/org/ohrim/taskmanagementsystem/TestTaskManagementSystemApplication.java
@@ -0,0 +1,11 @@
+package org.ohrim.taskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(TaskManagementSystemApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
