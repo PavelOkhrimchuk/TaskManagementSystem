816171533a664c33d0289312b2992589526125b9
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-20 21:15:27 +0300

    feat: comment entity first implementation

---
+++ src/main/java/org/ohrim/taskmanagementsystem/entity/Comment.java
@@ -0,0 +1,34 @@
+package org.ohrim.taskmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
