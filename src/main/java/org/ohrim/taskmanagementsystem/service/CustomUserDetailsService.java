ad67083d22b9f7972daa42f4f0bf0612b1e8652f
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-21 23:24:03 +0300

    feat: CustomUserDetailsService implementation

---
+++ src/main/java/org/ohrim/taskmanagementsystem/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.ohrim.taskmanagementsystem.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ohrim.taskmanagementsystem.entity.User;
+import org.ohrim.taskmanagementsystem.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
