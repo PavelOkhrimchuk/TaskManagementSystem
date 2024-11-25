ea2a6d0e69fe7746b95af7648bda04e8141adbb0
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-25 21:19:01 +0300

    feat: dtos integration in the CommentController

--- src/main/java/org/ohrim/taskmanagementsystem/controller/CommentController.java
+++ src/main/java/org/ohrim/taskmanagementsystem/controller/CommentController.java
@@ -2,7 +2,11 @@ package org.ohrim.taskmanagementsystem.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.ohrim.taskmanagementsystem.dto.comment.CommentPageResponse;
+import org.ohrim.taskmanagementsystem.dto.comment.CommentRequest;
+import org.ohrim.taskmanagementsystem.dto.comment.CommentResponse;
 import org.ohrim.taskmanagementsystem.entity.Comment;
+import org.ohrim.taskmanagementsystem.mapper.CommentMapper;
 import org.ohrim.taskmanagementsystem.service.CommentService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -15,29 +19,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
+    private final CommentMapper commentMapper;
 
 
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PostMapping("/{taskId}")
-    public ResponseEntity<Comment> addComment(@PathVariable Long taskId,
-                                              @RequestParam String content,
-                                              Authentication auth) {
+    public ResponseEntity<CommentResponse> addComment(
+            @PathVariable Long taskId,
+            @RequestBody CommentRequest commentRequest,
+            Authentication auth) {
         String authorEmail = auth.getName();
-        Comment comment = commentService.addComment(taskId, content, authorEmail);
-        return ResponseEntity.ok(comment);
-    }
+        Comment comment = commentService.addComment(taskId, commentRequest.getContent(), authorEmail);
 
+        CommentResponse commentResponse = commentMapper.toCommentResponse(comment);
+        return ResponseEntity.ok(commentResponse);
+    }
 
 
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping("/{taskId}")
-    public ResponseEntity<Page<Comment>> getCommentsByTask(
+    public ResponseEntity<CommentPageResponse> getCommentsByTask(
             @PathVariable Long taskId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         Page<Comment> comments = commentService.getCommentsByTask(taskId, page, size);
-        return ResponseEntity.ok(comments);
+
+        CommentPageResponse commentListResponse = commentMapper.toCommentPageResponse(comments);
+        return ResponseEntity.ok(commentListResponse);
     }
+
 
 
 
