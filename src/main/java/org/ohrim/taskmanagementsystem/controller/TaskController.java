50feab87e247b815def5b0ebf92535e014e9b904
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-25 21:18:41 +0300

    feat: dtos integration in the TaskController

--- src/main/java/org/ohrim/taskmanagementsystem/controller/TaskController.java
+++ src/main/java/org/ohrim/taskmanagementsystem/controller/TaskController.java
@@ -1,9 +1,12 @@
 package org.ohrim.taskmanagementsystem.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.ohrim.taskmanagementsystem.dto.TaskRequest;
+import org.ohrim.taskmanagementsystem.dto.task.PaginatedTasksResponse;
+import org.ohrim.taskmanagementsystem.dto.task.TaskRequest;
+import org.ohrim.taskmanagementsystem.dto.task.TaskResponse;
 import org.ohrim.taskmanagementsystem.entity.Task;
 import org.ohrim.taskmanagementsystem.entity.task.Status;
+import org.ohrim.taskmanagementsystem.mapper.TaskMapper;
 import org.ohrim.taskmanagementsystem.service.TaskService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -17,44 +20,42 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
 
     private final TaskService taskService;
+    private final TaskMapper taskMapper;
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody TaskRequest taskRequest, Authentication authentication) {
+    public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest taskRequest, Authentication authentication) {
         String authorEmail = authentication.getName();
-        Task task = taskService.createTask(
-                taskRequest.getTitle(),
-                taskRequest.getDescription(),
-                taskRequest.getPriority(),
-                authorEmail,
-                taskRequest.getExecutorEmail()
-        );
-        return ResponseEntity.ok(task);
+        Task task = taskService.createTask(taskRequest, authorEmail);
+        TaskResponse taskResponse = taskMapper.toTaskResponse(task);
+        return ResponseEntity.ok(taskResponse);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{taskId}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody TaskRequest taskRequest) {
-        Task task = taskService.updateTask(taskId, taskRequest.getTitle(),
-                taskRequest.getDescription(), taskRequest.getPriority(),
-                taskRequest.getExecutorEmail());
-        return ResponseEntity.ok(task);
+    public ResponseEntity<TaskResponse> updateTask(@PathVariable Long taskId, @RequestBody TaskRequest taskRequest) {
+        Task task = taskService.updateTask(taskId, taskRequest);
+        TaskResponse taskResponse = taskMapper.toTaskResponse(task);
+        return ResponseEntity.ok(taskResponse);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{taskId}/executor")
-    public ResponseEntity<Task> assignExecutor(@PathVariable Long taskId, @RequestParam String executorEmail) {
+    public ResponseEntity<TaskResponse> assignExecutor(@PathVariable Long taskId, @RequestParam String executorEmail) {
         Task task = taskService.assignExecutor(taskId, executorEmail);
-        return ResponseEntity.ok(task);
+        TaskResponse taskResponse = taskMapper.toTaskResponse(task);
+        return ResponseEntity.ok(taskResponse);
     }
 
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PatchMapping("/{taskId}/status")
-    public ResponseEntity<Task> changeStatus(@PathVariable Long taskId, @RequestParam Status status,
-                                             Authentication authentication) {
+    public ResponseEntity<TaskResponse> changeStatus(@PathVariable Long taskId, @RequestParam Status status,
+                                                     Authentication authentication) {
         Task task = taskService.changeStatus(taskId, status, authentication.getName());
-        return ResponseEntity.ok(task);
+        TaskResponse taskResponse = taskMapper.toTaskResponse(task);
+        return ResponseEntity.ok(taskResponse);
     }
+
 
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -66,22 +67,24 @@ public class TaskController {
 
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping("/author")
-    public ResponseEntity<Page<Task>> getTasksByAuthor(
+    public ResponseEntity<PaginatedTasksResponse> getTasksByAuthor(
             @RequestParam String authorEmail,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-        Page<Task> tasks = taskService.getTasksByAuthor(authorEmail, page, size);
-        return ResponseEntity.ok(tasks);
+        Page<Task> tasksPage = taskService.getTasksByAuthor(authorEmail, page, size);
+        PaginatedTasksResponse taskListResponse = taskMapper.toPaginatedTasksResponse(tasksPage);
+        return ResponseEntity.ok(taskListResponse);
     }
 
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping("/executor")
-    public ResponseEntity<Page<Task>> getTasksByExecutor(
+    public ResponseEntity<PaginatedTasksResponse> getTasksByExecutor(
             @RequestParam String executorEmail,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-        Page<Task> tasks = taskService.getTasksByExecutor(executorEmail, page, size);
-        return ResponseEntity.ok(tasks);
+        Page<Task> tasksPage = taskService.getTasksByExecutor(executorEmail, page, size);
+        PaginatedTasksResponse taskListResponse = taskMapper.toPaginatedTasksResponse(tasksPage);
+        return ResponseEntity.ok(taskListResponse);
     }
 
 
