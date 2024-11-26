cd8c2325627e16a32af4f788b583966062405130
Ohrim <okhrimchuk.pavel2005@gmail.com>
2024-11-27 02:59:34 +0300

    feat: GlobalExceptionHandler updated

---
+++ src/main/java/org/ohrim/taskmanagementsystem/configuration/exception/GlobalExceptionHandler.java
@@ -0,0 +1,112 @@
+package org.ohrim.taskmanagementsystem.configuration.exception;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.ohrim.taskmanagementsystem.dto.ErrorResponse;
+import org.ohrim.taskmanagementsystem.exception.task.InvalidRequestException;
+import org.ohrim.taskmanagementsystem.exception.task.ResourceNotFoundException;
+import org.ohrim.taskmanagementsystem.exception.task.TaskStatusException;
+import org.ohrim.taskmanagementsystem.exception.user.EmailAlreadyTakenException;
+import org.ohrim.taskmanagementsystem.exception.user.InvalidCredentialsException;
+import org.ohrim.taskmanagementsystem.exception.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.SignatureException;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    protected ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                status.value(),
+                message,
+                Instant.now()
+        );
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        StringBuilder errors = new StringBuilder();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+                errors.append(fieldError.getDefaultMessage()).append(" ")
+        );
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, errors.toString().trim());
+    }
+
+
+    @ExceptionHandler(EmailAlreadyTakenException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyTakenException(EmailAlreadyTakenException ex) {
+        return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ErrorResponse> handleSignatureException(SignatureException ex) {
+
+        String errorMessage = "Invalid JWT signature: " + ex.getMessage();
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, errorMessage);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientAuthenticationException(InsufficientAuthenticationException ex) {
+
+        String errorMessage = "Full authentication is required to access this resource";
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, errorMessage);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ErrorResponse> handleMalformedJwtException(MalformedJwtException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Malformed JWT token: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Expired JWT token: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ResponseEntity<ErrorResponse> handleUnsupportedJwtException(UnsupportedJwtException ex) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, "Unsupported JWT token: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRequest(InvalidRequestException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(TaskStatusException.class)
+    public ResponseEntity<ErrorResponse> handleTaskStatusException(TaskStatusException ex) {
+        return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+
+
+}
